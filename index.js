f64dbef9378747439f0c5258493b1510c0de9397
ArseniyFedarynchyk <92312125+ArseniyFedarynchyk@users.noreply.github.com>
2022-10-09 12:05:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
+const renderBtn = document.querySelector(".btn")
+const firstPassword = document.querySelector(".first-password")
+const secondPassword = document.querySelector(".second-password")
+
+renderBtn.addEventListener('click', function() {
+    firstPassword.textContent = ""
+    secondPassword.textContent = ""
+    const inputValue = document.getElementById("password-length").value
+
+    for (let i = 0; i < inputValue; i++) {
+        firstPassword.textContent += characters[Math.floor(Math.random() * characters.length)]
+        secondPassword.textContent += characters[Math.floor(Math.random() * characters.length)]
+    }
+})
